961954d9b5511872c054847c478331ac269e6aff
Ubuntu <ubuntu@ip-172-31-36-94.us-east-2.compute.internal>
2021-09-22 07:03:54 +0000

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-class HelloWorld {
+lass HelloWorld {
 	    public static void main(String[] args) {
-		            System.out.println("Hello, World!"); 
+		            System.out.println("hi everyone 12345!"); 
 			        }
 }
